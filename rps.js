79f2270184d2da54450bade6d0d7cea526e44391
Abbas4d4 <Abbas.Darboe@ada.ac.uk>
2024-10-16 19:33:41 +0000

    Implementing loop

--- rps.js
+++ rps.js
@@ -1,4 +1,5 @@
 
+/*
 //compares options and returns winning value
 function rockPaperScissors(player1, player2,same) {
 
@@ -25,6 +26,10 @@ function rockPaperScissors(player1, player2,same) {
 
 
 //calls functions and logs their outputs
+
+for(let i = 0; 1 = 8; i++) {
+
+}
 console.log(rockPaperScissors('rock','scissors'));
 console.log(rockPaperScissors('rock','paper','same'));
 console.log(rockPaperScissors('rock','rock','same'));
@@ -34,6 +39,44 @@ console.log(rockPaperScissors('scissors','rock'));
 console.log(rockPaperScissors('paper','paper','same'));
 console.log(rockPaperScissors('paper','scissors'));
 console.log(rockPaperScissors('paper','rock'));
+
+
+
+
+*/
+
+
+function check_winner(p1_pick,p2_pick) {
+  console.log(p1_pick,p2_pick)
+}
+
+player1_options = ['rock','paper','scissors']
+player2_options = ['rock','paper','scissors']
+index = 0
+
+for (let i = 0; i=8; i++){
+  check_winner(player1_options[index],player2_options[index] )
+  index += 1
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Leave this code here for the automated tests
 module.exports = {
