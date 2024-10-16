2648f393c2c2ed0ed48ee28047cdd29d88b2390d
Abbas4d4 <Abbas.Darboe@ada.ac.uk>
2024-10-16 23:09:21 +0000

    near complete

--- rps.js
+++ rps.js
@@ -1,72 +1,24 @@
-
-/*
-//compares options and returns winning value
-function rockPaperScissors(player1, player2,same) {
-
-   if (player1 === 'rock' && player2 === 'scissors'){
-     return player1
-    } else if (player1 === 'rock' && player2 === 'paper') {
-      return player2
-    } else if (player1 === 'rock' && player2 === 'rock') {
-      return same
-    } else if (player1 === 'scissors' && player2 === 'scissors'){
-      return same
-    } else if(player1 === 'scissors' && player2 === 'paper'){
-      return player1
-    } else if(player1 === 'scissors' && player2 ==='rock'){
-      return player2
-    } else if(player1 === 'paper' && player2 ==='scissors'){
-      return player2
-    } else if(player1 === 'paper' && player2 ==='paper'){
-      return same
-    } else if(player1 === 'paper' && player2 ==='rock'){
-      return player1
-    }
-};
-
-
-//calls functions and logs their outputs
-
-for(let i = 0; 1 = 8; i++) {
-
-}
-console.log(rockPaperScissors('rock','scissors'));
-console.log(rockPaperScissors('rock','paper','same'));
-console.log(rockPaperScissors('rock','rock','same'));
-console.log(rockPaperScissors('scissors','scissors','same'));
-console.log(rockPaperScissors('scissors','paper'));
-console.log(rockPaperScissors('scissors','rock'));
-console.log(rockPaperScissors('paper','paper','same'));
-console.log(rockPaperScissors('paper','scissors'));
-console.log(rockPaperScissors('paper','rock'));
-
-
-
-
-*/
-
-
-
-
 function rockPaperScissors(player1,player2) {
-   console.log(player1,player2)
-}
 
-player1_options = ['rock','paper','scissors']
-player2_options = ['rock','paper','scissors']
-
-for (let x = 0; x < player1_options.length; x++){
-  for (let y = 0; y < player2_options.length; y++) {
-      rockPaperScissors(player1_options[x], player2_options[y]);
+  //States which combinations result in player1 winning
+  var Player1_WinningCondition = {
+    'rock':['scissors','lizard'], 'paper':['rock','spock'], 'scissors':['paper','lizard'], 'lizard':['spock','paper'],'spock':['scissors','rock']
   }
-}
 
+  if (player1 === player2) {return 'draw'} 
+  else if (Player1_WinningCondition[player1].includes(player2)) {return 'player1'} 
+  else {return 'player2'};
+ }
+ 
 
+ var players_options = ['rock','paper','scissors','lizard','spock']
 
-
-
-
-
+ //loops through all combinations between players from given list(25)
+ for (let x = 0; x <  players_options.length; x++){   
+   for (let y = 0; y <  players_options.length; y++) {
+      var result = rockPaperScissors(players_options[x], players_options[y]);
+   }
+ }
 
 
 
