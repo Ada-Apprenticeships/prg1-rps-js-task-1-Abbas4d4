0741d6bb913acde99a18c6ab8ed15aae8fe1f121
Abbas4d4 <Abbas.Darboe@ada.ac.uk>
2024-10-17 14:20:25 +0000

    final commit

--- rps.js
+++ rps.js
@@ -1,23 +1,19 @@
 function rockPaperScissors(player1,player2) {
 
   //States which combinations result in player1 winning
-  var Player1_WinningCondition = {
-    'rock':['scissors','lizard'], 
-    'paper':['rock','spock'], 
-    'scissors':['paper','lizard'], 
-    'lizard':['spock','paper'],
-    'spock':['scissors','rock']
+  const Player1_WinningCondition = {
+    rock:['scissors','lizard'], 
+    paper:['rock','spock'], 
+    scissors:['paper','lizard'], 
+    lizard:['spock','paper'],
+    spock:['scissors','rock']
   }
 
-  if (player1 === player2) 
-  {
-    return 'draw'; 
-  } 
-  else 
-  {
-    return Player1_WinningCondition[player1].includes(player2) ?  'player1' :  'player2';  
+  if (player1 === player2) return 'draw'
 
-  }
+  return Player1_WinningCondition[player1].includes(player2) ?  'player1' :  'player2';  
+
+ 
 }
 
 
